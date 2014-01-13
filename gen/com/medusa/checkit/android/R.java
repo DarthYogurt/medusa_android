e7009cb225d953219e1af74e775a8db0c6f94870
Deric Walintukan <deric.walintukan@gmail.com>
2014-01-12 23:06:54 -0800

    main menu activity created

--- gen/com/medusa/checkit/android/R.java
+++ gen/com/medusa/checkit/android/R.java
@@ -21,11 +21,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int splash_screen_logo=0x7f020001;
+        public static final int btn_white_glossy=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int splash_screen_logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080002;
+        public static final int continue_checklist_button=0x7f080001;
+        public static final int new_checklist_button=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main_menu=0x7f030000;
@@ -38,7 +41,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int continue_checklist=0x7f050005;
         public static final int logo_description=0x7f050002;
+        public static final int new_checklist=0x7f050004;
         public static final int title_activity_main_menu=0x7f050003;
     }
     public static final class style {
--- src/com/medusa/checkit/android/MainMenuActivity.java
+++ src/com/medusa/checkit/android/MainMenuActivity.java
@@ -1,15 +1,31 @@
 package com.medusa.checkit.android;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainMenuActivity extends Activity {
+	
+	Intent newChecklistIntent;
+	String allChecklistsJSONString;
+	ArrayList<String[]> checklistsArray;
+	ArrayList<String[]> stepsArray;
 
+	@SuppressWarnings("unchecked")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		
+		checklistsArray = (ArrayList<String[]>) this.getIntent().getSerializableExtra("checklists");
+        stepsArray = (ArrayList<String[]>) this.getIntent().getSerializableExtra("steps");
+        
+//		newChecklistIntent = new Intent(this, NewChecklistActivity.class);
+//		newChecklistIntent.putExtra("checklists", checklistsArray);
+//		newChecklistIntent.putExtra("steps", stepsArray);
 	}
 
 	@Override
