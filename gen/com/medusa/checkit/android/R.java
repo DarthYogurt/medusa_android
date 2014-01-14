0ca1636f427d394e1e0a37d17cb0b178e7a8eada
Deric Walintukan <deric.walintukan@gmail.com>
2014-01-14 12:04:45 -0800

    some styling to listview row

--- gen/com/medusa/checkit/android/R.java
+++ gen/com/medusa/checkit/android/R.java
@@ -26,11 +26,12 @@ public final class R {
         public static final int splash_screen_logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080004;
+        public static final int action_settings=0x7f080005;
         public static final int checklist_id=0x7f080002;
         public static final int checklist_listview=0x7f080000;
         public static final int checklist_name=0x7f080001;
-        public static final int num_of_steps=0x7f080003;
+        public static final int num_of_steps=0x7f080004;
+        public static final int num_of_steps_label=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main_menu=0x7f030000;
@@ -45,8 +46,10 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int continue_checklist=0x7f050005;
+        public static final int id_label=0x7f050006;
         public static final int logo_description=0x7f050002;
         public static final int new_checklist=0x7f050004;
+        public static final int num_of_steps_label=0x7f050007;
         public static final int title_activity_main_menu=0x7f050003;
     }
     public static final class style {
